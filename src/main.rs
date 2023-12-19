6bea075669579d97a98d201dd9a86a17c7c93857
Darko Ivkov <bVNyDUKE@protonmail.com>
2023-12-19 23:10:39 +0100

    working

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs, i64};
 
-fn convert_value(val: i64, valmap: Vec<Vec<i64>>) -> i64 {
+fn convert_value(val: i64, valmap: &Vec<Vec<i64>>) -> i64 {
     for row in valmap {
         let (dest, src, rlen) = (row[0], row[1], row[2]);
         if src <= val && val < (src + rlen) {
@@ -9,6 +9,21 @@ fn convert_value(val: i64, valmap: Vec<Vec<i64>>) -> i64 {
     }
 
     return val;
+}
+
+fn parse_map(lines: &Vec<Vec<&str>>, idx: usize) -> Vec<Vec<i64>> {
+    let x: Vec<Vec<i64>> = lines
+        .get(idx)
+        .unwrap()
+        .iter()
+        .filter(|x| !x.contains("map"))
+        .map(|x| {
+            x.split_whitespace()
+                .map(|x| x.parse::<i64>().unwrap())
+                .collect()
+        })
+        .collect();
+    x
 }
 
 fn main() {
@@ -29,6 +44,34 @@ fn main() {
         .map(|el| el.trim().split("\n").collect())
         .collect();
 
-    println!("{:?}", lines);
-    println!("{:?}", seeds);
+    let soilmap = parse_map(&lines, 0);
+    let fertmap = parse_map(&lines, 1);
+    let watermap = parse_map(&lines, 2);
+    let lightmap = parse_map(&lines, 3);
+    let tempmap = parse_map(&lines, 4);
+    let humidmap = parse_map(&lines, 5);
+    let locmap = parse_map(&lines, 6);
+
+    let seed_location = |seed: i64| -> i64 {
+        let soil = convert_value(seed, &soilmap);
+        let fert = convert_value(soil, &fertmap);
+        let water = convert_value(fert, &watermap);
+        let light = convert_value(water, &lightmap);
+        let temp = convert_value(light, &tempmap);
+        let hum = convert_value(temp, &humidmap);
+        let loc = convert_value(hum, &locmap);
+        return loc;
+    };
+
+    let res = seeds.iter().fold(-1, |acc, &x| {
+        let val = seed_location(x);
+        println!("{}", val);
+        if acc == -1 || val < acc {
+            val
+        } else {
+            acc
+        }
+    });
+
+    println!("res: {}", res)
 }
